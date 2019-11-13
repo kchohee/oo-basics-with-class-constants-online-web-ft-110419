b3348168da012e8903b437495a18bc64577d96d9
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 22:29:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+<<<<<<< HEAD
   BRANDS = []
 
+=======
+  BRAND = []
+>>>>>>> 330e8caa926b7c176e1be01b2509ee381e048a92
   def initialize(brand)
    @brand = brand
     BRANDS << brand unless BRANDS.include?(brand)
@@ -13,4 +17,11 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+<<<<<<< HEAD
+=======
+  def brand=(brand)
+    @brand = brand
+    BRAND << @condition = "unique"
+  end
+>>>>>>> 330e8caa926b7c176e1be01b2509ee381e048a92
 end
